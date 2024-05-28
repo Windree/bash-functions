00659326e4dcf7e0bd2163477ef07371c63e5691
Windree <windree@dragonsoul.com>
2024-05-28 06:42:54 +0000

    fix: handle no argument list error

--- array_contains.sh
+++ array_contains.sh
@@ -2,6 +2,13 @@
 set -Eeuo pipefail
 
 function array_contains() {
+    if [ "$#" -lt 1 ]; then
+        echo >&2 "array_contains requires at least one aragument"
+        exit 255
+    fi
+    if [ "$#" -eq 1 ]; then
+        return 1
+    fi
     local value="${!#}"
     local argv=("${@:1:$#-1}")
     echo $argv | grep -w -q "$value"
